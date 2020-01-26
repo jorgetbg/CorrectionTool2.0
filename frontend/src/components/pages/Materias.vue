4768701a6afec9cc3465887ce2fae36d2fa1afb2
João Augusto Grobe Castilho <joaocastilho@alunos.utfpr.edu.br>
2020-01-26 10:14:32 -0300

    Expansion Panel em card de Matéria

--- frontend/src/components/pages/Materias.vue
+++ frontend/src/components/pages/Materias.vue
@@ -10,31 +10,39 @@
         </v-fab-transition>
       </v-layout>
 
-
       <v-row align-content="space-around">
         <v-col md="6" sm="12" class="px-2 py-1" v-for="(materia, i) in materias" :key="i">
-          <v-card flat :class="`${materia.status} materia px-2 my-1`">
-            <v-layout wrap class="pa-3">
-              <v-flex xs="12" md="4">
-                <div class="caption grey--text">Nome do matéria</div>
-                <div>{{materia.nome}}</div>
-              </v-flex>
-              <v-flex xs="6" md="1">
-                <div class="caption grey--text">Capacidade</div>
-                <div>{{materia.capacidade}}</div>
-              </v-flex>
-              <v-flex xs="2" sm="4" md="1">
-                <div class="caption grey--text">Status</div>
-                <v-switch
-                  right
-                  class="my-0"
-                  v-model="materia.status"
-                  disabled
-                  color="green"
-                ></v-switch>
-              </v-flex>
-            </v-layout>
-          </v-card>
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header class>
+                <v-card flat :class="`${materia.status} materia px-2 my-1`">
+                  <v-layout wrap class="pa-3">
+                    <v-flex xs="12" md="4">
+                      <div class="caption grey--text">Nome do matéria</div>
+                      <div>{{materia.nome}}</div>
+                    </v-flex>
+                    <v-flex xs="6" md="1">
+                      <div class="caption grey--text">Capacidade</div>
+                      <div>{{materia.lotacao}} / {{materia.capacidade}}</div>
+                    </v-flex>
+                    <v-flex xs="2" sm="4" md="1">
+                      <div class="caption grey--text">Status</div>
+                      <v-switch class="my-0" v-model="materia.status" disabled color="green"></v-switch>
+                    </v-flex>
+                  </v-layout>
+                </v-card>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-layout nowrap overflow-hidden>
+                    <v-flex v-for="(item, i) in materia.lotacao" :key="i">
+                      <div>
+                        <v-icon d-inline>person</v-icon>
+                      </div>
+                    </v-flex>
+                </v-layout>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
         </v-col>
       </v-row>
     </v-container>
@@ -47,10 +55,25 @@ export default {
   data() {
     return {
       materias: [
-        { nome: "Engenharia Prod. 2020/1", capacidade: "12/34", status: true },
-        { nome: "BCC. 2020/1", capacidade: "42/42", status: true },
-        { nome: "Engenharia Mec. 2020/1", capacidade: "7/34", status: true },
-        { nome: "Engenharia Elet. 2019/2", capacidade: "30/34", status: false }
+        {
+          nome: "Engenharia Prod. 2020/1",
+          capacidade: 34,
+          status: true,
+          lotacao: 12
+        },
+        { nome: "BCC. 2020/1", capacidade: 42, status: true, lotacao: 8 },
+        {
+          nome: "Engenharia Mec. 2020/1",
+          capacidade: 34,
+          status: true,
+          lotacao: 27
+        },
+        {
+          nome: "Engenharia Elet. 2019/2",
+          capacidade: 34,
+          status: false,
+          lotacao: 33
+        }
       ]
     };
   }
@@ -58,11 +81,10 @@ export default {
 </script>
 
 <style>
-
-.materia.true{
+.materia.true {
   border-left: 4px solid #3cd1c2;
 }
-.materia.false{
+.materia.false {
   border-left: 4px solid tomato;
 }
 </style>
