9db7cac2f80d5812da10fc5a7c3011b8cf4478e0
João Augusto Grobe Castilho <joaocastilho@alunos.utfpr.edu.br>
2020-02-10 16:53:57 -0300

    Melhoria na rota de Obter Matriculas
    Rota retorna informação sobre exercicios e entregas do aluno em relação a matéria matriculada.
    Removido o map para excluir campos indesejados, agora esta sendo usado $project

--- backend/src/controllers/MatriculaController.js
+++ backend/src/controllers/MatriculaController.js
@@ -49,21 +49,38 @@ module.exports = {
     const itensPorPagina = 10
     let matriculas
     try{
-      matriculas = await Matricula.find({aluno:req.body.userId}).limit(itensPorPagina).skip(itensPorPagina * (pagina - 1)).populate('materia', 'nome') //populate esta causando +150ms
+      //matriculas = await Matricula.find({aluno:req.body.userId}, "materia").limit(itensPorPagina).skip(itensPorPagina * (pagina - 1)).populate('materia', "nome professor") //populate esta causando +150ms
+      var id = require('mongodb').ObjectID(req.body.userId);
+      matriculas = await Matricula.aggregate([
+        {$match: {aluno: id}},
+        {$limit: itensPorPagina},
+        {$skip: itensPorPagina * (pagina - 1)},
+        {$lookup: {
+          "from": "materias",
+          "localField": "materia",
+          "foreignField": "_id",
+          "as": "materia"
+        }},
+        {$lookup: {
+          "from": "exercicios",
+          "localField": "materia._id",
+          "foreignField": "materia",
+          "as": "exercicios"
+        }},
+        {$lookup: {
+          "from": "resolucaos",
+          "localField": "exercicios._id",
+          "foreignField": "exercicio",
+          "as": "resolucoes"
+        }},
+        {$set: {"exercicios": {$size:"$exercicios"}}},
+        {$set: {"resolucoes": {$size:"$resolucoes"}}},
+        {$project: {"materia.password": 0, "materia.capacidade": 0,"materia.lotacao": 0, "materia.__v": 0, "__v": 0, "aluno": 0}}
+      ])
     }catch(e){
       return res.status(400).send({ status: "error", message: e, data: null })
     }
-    console.log(matriculas)
 
-    matriculas = matriculas.map(matricula => {
-      return {
-        materia: {
-          _id: matricula.materia._id,
-          nome:  matricula.materia.nome
-        }
-
-      }
-    })
     return res.status(200).send({ status: "success", message: "Matriculas obtidas com sucesso!!!", data: {matriculas}})
   }
 };
